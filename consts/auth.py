9e05186141d8b42c886937c35b4a152dc191bebd
Martin Infante <maleinf09@gmail.com>
2022-03-20 15:51:45 -0300

    Reduce gmail scope

--- consts/auth.py
+++ consts/auth.py
@@ -1,6 +1,6 @@
 class Auth:
     CLIENT_SECRET_FILE = "utils/files/client_secret.json"
     CREDENTIALS_FILE = "utils/files/credentials.json"
-    SCOPES = ['https://mail.google.com',
+    SCOPES = ['https://www.googleapis.com/auth/gmail.send',
               'https://www.googleapis.com/auth/drive',
               'https://www.googleapis.com/auth/calendar']
